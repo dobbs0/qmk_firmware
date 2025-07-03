1105f5d334639ae4b1a72a4159449311fdcf7d68
Dobbs <fyrix@outlook.com>
2025-07-03 11:46:30 -0700

    changed stm32f411ce

--- keyboards/0dobbs_stm32f411ce/halconf.h
+++ keyboards/0dobbs_stm32f411ce/halconf.h
@@ -2,7 +2,7 @@
 
 // #define HAL_USE_PWM TRUE
 // #define HAL_USE_SERIAL TRUE
-// #define HAL_USE_SPI TRUE
+#define HAL_USE_SPI TRUE
 // #define SPI_USE_WAIT TRUE
 // #define SPI_SELECT_MODE SPI_SELECT_MODE_PAD
 
